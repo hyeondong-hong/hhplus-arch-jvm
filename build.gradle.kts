f2ea4dc930adbb96cf6f03b2d1cfb9f543ee93e8
hyeondong-hong <h2dong@kakao.com>
2024-09-29 17:30:43 +0900

    init

---
+++ build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.3.4"
+    id("io.spring.dependency-management") version "1.1.6"
+    id("jacoco")
+}
+
+group = "io.hhplus"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    compileOnly("org.projectlombok:lombok")
+    runtimeOnly("com.h2database:h2")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+with(extensions.getByType(JacocoPluginExtension::class.java)) {
+    toolVersion = "0.8.7"
+}
+
+tasks.getByName("bootJar") {
+    enabled = true
+}
+
+tasks.getByName("jar") {
+    enabled = false
+}
+
+tasks.test {
+    ignoreFailures = true
+    useJUnitPlatform()
+}
